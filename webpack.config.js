8075cb0f28213b2043dfa920e3ee93ec36b7de83
ideen-los <90513724+ideen-los@users.noreply.github.com>
2024-02-25 21:30:21 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Test",
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // Injects CSS into the DOM
+          "css-loader", // Turns CSS into commonjs
+          "sass-loader", // Turns SCSS into CSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
